c2e8cac0b07f28796d2325921b004395e129b869
isaac <baesh@gabia.com>
2021-02-14 23:01:58 +0900

    다대다 관계 추가

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { resolve } = require("path");
 
 const path = resolve(__dirname, "..", "schema", "graph.graphql");
 
-const typeDefs = fs.readFileSync(path, { encoding: "utf8", });
+const typeDefs = fs.readFileSync(path, { encoding: "utf8" });
 
 var _id = 0;
 var photos = [
@@ -46,6 +46,13 @@ var users = [
   },
 ];
 
+var tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" },
+  { photoID: "2", userID: "gPlake" },
+];
+
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
@@ -66,11 +73,21 @@ const resolvers = {
     postedBy: (parent) => {
       return users.find((u) => u.githubLogin === parent.githubUser);
     },
+    taggedUsers: (parent) =>
+      tags
+        .filter((tag) => tag.photoID === parent.id)
+        .map((tag) => tag.userID)
+        .map((userID) => users.find((u) => u.githubLogin === userID)),
   },
   User: {
     postedPhotos: (parent) => {
       return photos.filter((p) => p.githubUser === parent.githubLogin);
     },
+    inPhotos: (parent) =>
+      tags
+        .filter((tag) => tag.userID === parent.id)
+        .map((tag) => tag.photoID)
+        .map((photoID) => photos.find((p) => p.id === photoID)),
   },
 };
 
