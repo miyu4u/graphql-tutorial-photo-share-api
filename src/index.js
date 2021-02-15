d376006ea4f5bff2bf6ff927df7564cecfe9cad8
isaac <baesh@gabia.com>
2021-02-15 20:50:01 +0900

    mongodb connection 추가

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-const express = require("express");
-const { ApolloServer, chainResolvers } = require("apollo-server-express");
-const fs = require("fs");
-const { GraphQLScalarType } = require("graphql");
-const { resolve } = require("path");
-const expressPlayground = require("graphql-playground-middleware-express");
+const express = require("express")
+const { ApolloServer, chainResolvers } = require("apollo-server-express")
+const fs = require("fs")
+const { GraphQLScalarType } = require("graphql")
+const { resolve } = require("path")
+const expressPlayground = require("graphql-playground-middleware-express")
+require("dotenv").config()
 
-const path = resolve(__dirname, "..", "schema", "graph.graphql");
+const path = resolve(__dirname, "..", "schema", "graph.graphql")
 
-const typeDefs = fs.readFileSync(path, { encoding: "utf8" });
+const typeDefs = fs.readFileSync(path, { encoding: "utf8" })
 
-const resolvers = require("./resolvers");
+const resolvers = require("./resolvers")
+const { MongoClient } = require("mongodb")
 
-const app = express();
+async function bootstrap() {
+	const app = express()
+	const MONGO_DB = process.env.DB_HOST
+	const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+	const db = client.db()
+	const context = { db }
+	const server = new ApolloServer({ typeDefs, resolvers, context })
+	server.applyMiddleware({ app })
+	app.get("/", (req, res) => res.send("PhotoShare API에 오신것을 환영합니다"))
+	app.get("/playground", expressPlayground.default({ endpoint: "/graphql" }))
 
-const server = new ApolloServer({ typeDefs, resolvers });
+	app.listen({ port: 4000 }, () => console.log(`GraphQL Service running on ${server.graphqlPath}`))
+}
 
-server.applyMiddleware({ app });
-
-app.get("/", (req, res) => res.send("PhotoShare API에 오신것을 환영합니다"));
-app.get("/playground", expressPlayground.default({ endpoint: "/graphql" }));
-
-app.listen({ port: 4000 }, () => console.log(`GraphQL Service running on ${server.graphqlPath}`));
+bootstrap()
